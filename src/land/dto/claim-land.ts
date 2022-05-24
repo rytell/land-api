e13bfb09758c05d52d81156b6da909d1dce622fb
Jesus Ovalles <jesuseovalles@gmail.com>
2022-05-24 10:35:45 -0400

    feat: add version to the land-api endpoint

--- src/land/dto/claim-land.ts
+++ src/land/dto/claim-land.ts
@@ -8,5 +8,6 @@ export class ClaimLandDto {
     heroNumber: number;
     owner: string;
     transactionHash?: string = '';
-    v2: boolean;
+    v2: boolean; //TODO: Delete this property when the implementation of the versions is completed.
+    version: string = "V3";
 }
